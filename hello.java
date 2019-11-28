f60f5db46a42fbde36fb9cb472be11c16855e5ae
dongzhongxin123 <40479698+dongzhongxin123@users.noreply.github.com>
2019-11-28 21:10:10 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,2 @@
 <<<<<<< HEAD
-this is my first java 111   £¡£¡
-first
-second
-=======
-this is my first java 111   Â£Â¡Â£Â¡
-error happened
->>>>>>> 728cf8afcb1fcf56325b65598ec71ebb15b04907
-error happend again
+this is my first java 111  
